9d9a238d8cf6bb603096a03d45d33970af3f33d4
xiarh <xiarunhao123@163.com>
2017-05-27 16:04:04 +0800

    天气Icon

---
+++ app/src/main/java/com/xiarh/purenews/util/WeatherUtil.java
@@ -0,0 +1,20 @@
+package com.xiarh.purenews.util;
+
+import android.widget.ImageView;
+
+/**
+ * Created by xiarh on 2017/5/27.
+ */
+
+public class WeatherUtil {
+
+    /**
+     * 设置天气图标
+     *
+     * @param code
+     * @param imageView
+     */
+    public static void setWeatherIcon(String code, ImageView imageView) {
+
+    }
+}
